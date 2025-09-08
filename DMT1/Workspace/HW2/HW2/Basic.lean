c65c2c3acd5c1ea34e4c3946df9ddcdcff11b123
unknown <sladjana.suzic@nhrec.org>
2025-09-08 17:47:54 -0400

    Finished HW2

--- DMT1/Workspace/HW2/HW2/Basic.lean
+++ DMT1/Workspace/HW2/HW2/Basic.lean
@@ -1,1 +1,23 @@
-def hello := "world"
+-- vtq5nn
+
+axiom P : Prop
+axiom Q : Prop
+
+/-
+If P ∧ Q, then we can conclude that
+Q ∧ P.
+
+Example:
+-/
+
+example (pq : P ∧ Q) : Q ∧ P :=
+⟨pq.right, pq.left⟩
+
+/-
+If we know P ∧ Q, then we can conclude P
+
+Example:
+-/
+
+example (p_left : P ∧ Q) : P :=
+p_left.left
